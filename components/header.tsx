6a9ef671ef622d33d3fd087d4d245b780c8162e6
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2023-01-07 12:13:25 -0300

    feat: add responsive mobile menu

--- components/header.tsx
+++ components/header.tsx
@@ -7,31 +7,50 @@ import c from "./header.module.css";
 export default function Header() {
   return (
     <>
-    <header className={c.header}>
-      <div className={c.header__content}>
-        <picture className={c.logo}>
-          <Link href="/" className={c.logo__full}>
-            <LogoHatM />
-          </Link>
-          <Link href="/" className={c.logo__hat_s}>
-            <HatS />
-          </Link>
-        </picture>
-        <nav className={c.nav}>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about-us">About Us</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+      <header className={c.header}>
+        <div className={c.header__content}>
+          <picture className={c.logo}>
+            <Link href="/" className={c.logo__full}>
+              <LogoHatM />
+            </Link>
+            <Link href="/" className={c.logo__hat_s}>
+              <HatS />
+            </Link>
+          </picture>
+          <nav className={c.nav}>
+            <ul>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about-us">About Us</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <nav className={c.mobile_nav}>
+            <input id="toggle-nav" type="checkbox" className={c.toggle} />
+            <label htmlFor="toggle-nav" className={c.toggle_label} tabindex="0">
+              Menu
+            </label>
+            <div className={c.toggle_content}>
+            <ul>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about-us">About Us</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
       <div className={c.header__bg}></div>
     </>
   );
