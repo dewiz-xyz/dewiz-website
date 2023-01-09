c302312befd5b257522786a2044d79e8b5d57e13
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2023-01-09 12:14:25 -0300

    feat: add cross-platform favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,27 @@ export default function Home() {
         <title>Dewiz | DeFi Engineering Services</title>
         <meta name="description" content="Dewiz | DeFi Engineering Services" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b2e2" />
+        <meta name="msapplication-TileColor" content="#001a1a" />
+        <meta name="theme-color" content="#001a1a" />
       </Head>
       <Header />
       <Main className={c.main}>
