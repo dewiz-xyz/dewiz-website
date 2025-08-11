c97834d80fbc1ef903ff62c48db6e73a7b653231
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2025-08-11 18:27:16 -0300

    refactor: use CSS-only mobile menu

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,8 @@ import LogoHatM from "../public/logo-hat-m.svg";
 import HatM from "../public/hat-m.svg";
 import HatS from "../public/hat-s.svg";
 import c from "./header.module.css";
-import { useState } from "react";
 
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
       <header className={c.header}>
@@ -33,35 +31,33 @@ export default function Header() {
             </ul>
           </nav>
           <nav className={c.mobile_nav} aria-label="Mobile navigation">
-            <button
-              className={`${c.hamburger_button} ${isMenuOpen ? c.open : ''}`}
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              aria-expanded={isMenuOpen}
-              aria-controls="mobile-nav-menu"
-            >
+            <input type="checkbox" id="menu-toggle" className={c.menu_toggle} aria-hidden="true" />
+            <label htmlFor="menu-toggle" className={c.hamburger_button} role="button" aria-controls="mobile-nav-menu">
               <span className={c.hamburger_line}></span>
               <span className={c.hamburger_line}></span>
               <span className={c.hamburger_line}></span>
               <span className="srOnly">Toggle Menu</span>
-            </button>
-            {isMenuOpen && (
-              <>
-                <div className={c.overlay} onClick={() => setIsMenuOpen(false)}></div>
-                <div className={c.menu} id="mobile-nav-menu">
-                  <ul>
-                    <li>
-                      <a href="#home" onClick={() => setIsMenuOpen(false)}>Home</a>
-                    </li>
-                    <li>
-                      <a href="#about-us" onClick={() => setIsMenuOpen(false)}>About Us</a>
-                    </li>
-                    <li>
-                      <a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              </>
-            )}
+            </label>
+            <label htmlFor="menu-toggle" className={c.overlay}></label>
+            <div className={c.menu} id="mobile-nav-menu">
+              <ul>
+                <li>
+                  <label htmlFor="menu-toggle">
+                    <a href="#home">Home</a>
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="menu-toggle">
+                    <a href="#about-us">About Us</a>
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="menu-toggle">
+                    <a href="#contact">Contact</a>
+                  </label>
+                </li>
+              </ul>
+            </div>
           </nav>
         </div>
       </header>
