315b9979dc60751a826cd0597f72c65bedf549ff
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2026-05-18 15:47:06 -0300

    Update team roster

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,8 @@ import LogoHatM from "../public/logo-hat-m.svg";
 import avatarAmusingaxl from "../public/avatar-amusingaxl.png";
 import avatar0x3phemeralsoul from "../public/avatar-0x3phemeralsoul.png";
 import avatar0xdecr1pto from "../public/avatar-0xdecr1pto.png";
-import avatarOddaf from "../public/avatar-oddaf.png";
-import avatar0xbasset from "../public/avatar-0xbasset.png";
 import avatar0xpedro from "../public/avatar-0xpedro.jpg";
+import avatar0xjoao from "../public/avatar-0xjoao.jpg";
 import avatar0xlaz3r from "../public/avatar-0xlaz3r.png";
 import avatarRiccardo from "../public/avatar-riccardo.png";
 import avatar0xcentur1on from "../public/avatar-0xcentur1on.png";
@@ -138,22 +137,6 @@ export default function Home() {
             />
             <TeamMemberCard
               className={c.grid__item}
-              img={avatarOddaf}
-              url="https://x.com/0x0ddaf"
-              name="Oddaf"
-              companyRole="Smart Contracts Engineer"
-              description="Solidity auditor/dev since 2017. DeFi builder. Previously in infosec for web2 companies. Avid motorcyclist."
-            />
-            <TeamMemberCard
-              className={c.grid__item}
-              img={avatar0xbasset}
-              url="https://x.com/0xbasset"
-              name="0xbasset"
-              companyRole="Smart Contracts Engineer"
-              description="7+ years of experience in Solidity development and blockchain engineering. Pioneer in fully on-chain games during NFT boom. Fitness enthusiast."
-            />
-            <TeamMemberCard
-              className={c.grid__item}
               img={avatar0xpedro}
               url="https://x.com/0xpedro_eth"
               name="0xpedro"
@@ -183,6 +166,14 @@ export default function Home() {
               name="0xCentur1on"
               companyRole="Smart Contracts / Backend Engineer"
               description="30+ years in software engineering. Previously in e-commerce and banking services. Shipping DeFi smart contracts since 2018. Golang, Solidity and Rust. &quot;Audentes Fortuna iuvat&quot;"
+            />
+            <TeamMemberCard
+              className={c.grid__item}
+              img={avatar0xjoao}
+              url="https://x.com/0xjoao_eth"
+              name="0xjoao"
+              companyRole="Smart Contracts Engineer"
+              description="Onchain since 2017, building Web3 systems for 5+ years. Experienced in DeFi protocol systems across real production environments. EVM, anime, superheroes, and football."
             />
           </div>
         </section>
